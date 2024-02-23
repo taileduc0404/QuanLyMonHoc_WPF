611c293d33cb1ac19935c953f45235fc780c4236
taileduc0404 <taileduc0404@gmail.com>
2024-02-23 09:20:49 +0700

    Add [WindowHocvien, WindowChinh]

---
+++ QuanLyMonHoc/WindowChinh.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyMonHoc
+{
+    /// <summary>
+    /// Interaction logic for WindowChinh.xaml
+    /// </summary>
+    public partial class WindowChinh : Window
+    {
+        public WindowChinh()
+        {
+            InitializeComponent();
+        }
+
+        private void menuMonHoc_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+
+        private void menuHocVien_Click(object sender, RoutedEventArgs e)
+        {
+            WindowHocvien windowHocvien = new WindowHocvien();
+            windowHocvien.Show();
+        }
+    }
+}
